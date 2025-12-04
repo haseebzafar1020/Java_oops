2462233a47d8c9bb58a3145290f0eb8524dd9da3
Haseeb Zafar 1020 <haseebmalikgoto@gmail.com>
2025-12-04 07:24:03 -0800

    packages

---
+++ Packages.java
@@ -0,0 +1,20 @@
+// packages
+ // there are two type of packages 
+ //1==> user defined packages
+ //2==> Builden packages
+
+ // kuch asi chezy jo apko bar bar karni paray tu ap usko ak dafa likh kar package bana la or usy jab use karna hu tu import kar lain.
+ // is tarah kuch buildn aty hn java ki taraf se 
+
+
+import haseeb.acountInfo;
+
+public class Packages {
+    public static void main(String[] args) {
+      acountInfo obj = new acountInfo();
+       obj.name = "haseeb";
+       obj.age = 18;
+       obj.info();
+    }
+    
+}
