06c4776b8fe827d6b06498750b2b7b5249478da6
Haseeb Zafar 1020 <haseebmalikgoto@gmail.com>
2025-12-14 01:18:38 -0800

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,8 @@
+// ********** Encapsulation *****************
+// data ko hide karna 
+// like  ander ki cheezo ka na pata hu just bahir ki cheezo nazr ana
+
+public class Encapsulation {
+    
+
+}
